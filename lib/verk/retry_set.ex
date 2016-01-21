96a633c7c2eabf49b66f3e9101e5bef964286a3f
Eduardo Gurgel <eduardo@carnivallabs.com>
2016-01-22 10:34:21 +1300

    Change Verk.RetrySet functions to use Verk.Redis

--- lib/verk/retry_set.ex
+++ lib/verk/retry_set.ex
@@ -2,19 +2,19 @@ defmodule Verk.RetrySet do
   @retry_key "retry"
 
   @doc """
-  Count how many jobs are inside the retry set
+  Counts how many jobs are inside the retry set
   """
-  @spec count(pid) :: integer
-  def count(redis) do
-    Redix.command!(redis, ["ZCARD", @retry_key])
+  @spec count :: integer
+  def count do
+    Redix.command!(Verk.Redis, ["ZCARD", @retry_key])
   end
 
   @doc """
-  List jobs from `start` to `stop`
+  Lists jobs from `start` to `stop`
   """
-  @spec range(pid, integer, integer) :: [Verk.Job.T]
-  def range(redis, start \\ 0, stop \\ -1) do
-    for job <- Redix.command!(redis, ["ZRANGE", @retry_key, start, stop]) do
+  @spec range(integer, integer) :: [Verk.Job.T]
+  def range(start \\ 0, stop \\ -1) do
+    for job <- Redix.command!(Verk.Redis, ["ZRANGE", @retry_key, start, stop]) do
       Poison.decode!(job, as: Verk.Job)
     end
   end
