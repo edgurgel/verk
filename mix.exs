d1d13a2eb727e82231d92c1237a40bc072c61c7f
Eduardo Gurgel <eduardo@gurgel.me>
2016-02-08 22:31:38 +1300

    Add redis_url and node_id to application env
    This configuration will be the default one so Verk will run without a
    configuration
    
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -20,6 +20,7 @@ defmodule Verk.Mixfile do
 
   def application do
     [applications: [:logger, :poison, :tzdata, :timex, :redix, :poolboy],
+     env: [node_id: "1", redis_url: "redis://127.0.0.1:6379"],
      mod: {Verk, []}]
   end
 
