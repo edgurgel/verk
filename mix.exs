d747c52ca13f1a65bdbf936854c5f36959fa8156
Eduardo Gurgel <eduardo@carnivallabs.com>
2016-03-29 14:15:34 +1300

    Bump version 0.9.11
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Verk.Mixfile do
 
   def project do
     [app: :verk,
-     version: "0.9.10",
+     version: "0.9.11",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
