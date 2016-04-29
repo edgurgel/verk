e8c97d92d637f6e9196c15521d86d1b0bc38baed
Mitchell Henke <mitchh23@gmail.com>
2016-04-29 15:21:55 -0500

    random timeout

--- test/workers_manager_test.exs
+++ test/workers_manager_test.exs
@@ -125,6 +125,7 @@ defmodule Verk.WorkersManagerTest do
   end
 
   test "handle info timeout with free workers and no jobs", %{ monitors: monitors } do
+    :random.seed(1000)
     queue_manager_name = :queue_manager_name
     timeout = 1000
     state = %State{ monitors: monitors, pool_name: "pool_name",
@@ -132,12 +133,13 @@ defmodule Verk.WorkersManagerTest do
 
     expect(Verk.QueueManager, :dequeue, [queue_manager_name, 1], [])
 
-    assert handle_info(:timeout, state) == { :noreply, state, state.timeout }
+    assert handle_info(:timeout, state) == { :noreply, state, 1692 }
 
     assert validate Verk.QueueManager
   end
 
   test "handle info timeout with free workers and jobs to be done", %{ monitors: monitors } do
+    :random.seed(1000)
     queue_manager_name = :queue_manager_name
     pool_name = :pool_name
     timeout = 1000
@@ -154,7 +156,7 @@ defmodule Verk.WorkersManagerTest do
     expect(:poolboy, :checkout, [pool_name, false], worker)
     expect(Verk.Worker, :perform_async, [worker, worker, job], :ok)
 
-    assert handle_info(:timeout, state) == { :noreply, state, state.timeout }
+    assert handle_info(:timeout, state) == { :noreply, state, 1692 }
     assert match?([{^worker, ^job_id, ^job, _, _}], :ets.lookup(monitors, worker))
     assert_receive %Verk.Events.JobStarted{ job: ^job, started_at: _ }
 
