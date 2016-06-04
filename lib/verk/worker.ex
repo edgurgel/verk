41f177f151906e8426d0b3ae9d9a398723f43dbd
zhongwencool <zhongwencool@gmail.com>
2016-06-04 00:39:07 -0500

    Change Worker to find module with Module.safe_concat/1 (#72)

--- lib/verk/worker.ex
+++ lib/verk/worker.ex
@@ -33,7 +33,7 @@ defmodule Verk.Worker do
   def handle_cast({:perform, job, manager}, state) do
     try do
       :erlang.put(@process_dict_key, job)
-      apply(String.to_existing_atom("Elixir.#{job.class}"), :perform, job.args)
+      [job.class] |> Module.safe_concat |> apply(:perform, job.args)
       GenServer.cast(manager, {:done, self, job.jid})
       {:stop, :normal, state}
     rescue
--- test/worker_test.exs
+++ test/worker_test.exs
@@ -29,6 +29,24 @@ defmodule Verk.WorkerTest do
     assert_receive {:"$gen_cast", {:done, ^worker, "job_id"}}
   end
 
+  test "cast perform runs the specified atom module with the args succeding" do
+    worker = self
+    job = %Verk.Job{jid: "job_id", class: TestWorker, args: [worker]}
+    assert handle_cast({ :perform, job, worker }, :state) == { :stop, :normal, :state }
+
+    assert_receive :perform_executed
+    assert_receive {:"$gen_cast", {:done, ^worker, "job_id"}}
+  end
+
+  test "cast perform runs the no exist module with the args succeding" do
+    worker = self
+    job = %Verk.Job{jid: "job_id", class: TestWorkerNoExist, args: [worker]}
+    assert handle_cast({ :perform, job, worker }, :state) == { :stop, :failed, :state }
+
+    exception = %UndefinedFunctionError{arity: 1, function: :perform, module: TestWorkerNoExist, reason: nil}
+    assert_receive { :"$gen_cast", { :failed, ^worker, "job_id", ^exception, _ } }
+  end
+
   test "cast perform runs the specified module with the args failing" do
     worker = self
     job = %Verk.Job{jid: "job_id", class: "FailWorker", args: ["arg1"]}
