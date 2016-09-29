8f648b57182e800df08089d0d49d9e21700a80ec
Mitchell Henke <mitchh23@gmail.com>
2016-09-29 10:02:27 -0500

    fix long line

--- lib/verk/job.ex
+++ lib/verk/job.ex
@@ -5,7 +5,8 @@ defmodule Verk.Job do
 
   @default_max_retry_count Application.get_env(:verk, :max_retry_count, 25)
   @keys [error_message: nil, failed_at: nil, retry_count: 0, queue: nil, class: nil, args: [],
-         jid: nil, finished_at: nil, enqueued_at: nil, retried_at: nil, error_backtrace: nil, max_retry_count: @default_max_retry_count]
+         jid: nil, finished_at: nil, enqueued_at: nil, retried_at: nil, error_backtrace: nil,
+         max_retry_count: @default_max_retry_count]
 
   @derive {Poison.Encoder, only: Keyword.keys(@keys)}
   defstruct [:original_json | @keys]
