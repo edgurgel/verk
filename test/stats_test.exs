0af65904ffa193a7f309fd5bd2db857d78325a1a
Keyan Pishdadian <kpishdadian@gmail.com>
2016-10-25 22:09:47 -0400

    Add simple tests for queue_totals stats

--- test/stats_test.exs
+++ test/stats_test.exs
@@ -7,6 +7,7 @@ defmodule Verk.StatsTest do
                        |> elem(1)
                        |> Redix.start_link
     Redix.command!(redis, ~w(DEL stat:processed stat:failed))
+    Redix.command!(redis, ~w(DEL stat:processed:default stat:failed:default))
     { :ok, redis: redis }
   end
 
@@ -19,6 +20,18 @@ defmodule Verk.StatsTest do
       Redix.command!(redis, ~w(MSET stat:processed 25 stat:failed 32))
 
       assert total(redis) == %{ processed: 25, failed: 32 }
+    end
+  end
+
+  describe "queue_total/2" do
+    test "with no data", %{ redis: redis } do
+      assert queue_total("default", redis) == %{total_processed: 0, total_failed: 0}
+    end
+
+    test "with data", %{ redis: redis } do
+      Redix.command!(redis, ~w(MSET stat:processed:default 25 stat:failed:default 32))
+
+      assert queue_total("default", redis) == %{total_processed: 25, total_failed: 32}
     end
   end
 end
