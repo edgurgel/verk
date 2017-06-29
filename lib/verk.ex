d2c65dff441d03a9f9514586301f9d9ef9cfed19
Eduardo Gurgel <eduardo@carnivallabs.com>
2017-06-29 15:02:58 +1200

    Expose Verk.pause_queue & Verk.resume_queue

--- lib/verk.ex
+++ lib/verk.ex
@@ -28,6 +28,9 @@ defmodule Verk do
     Verk.Supervisor.stop_child(queue)
   end
 
+  defdelegate pause_queue(queue), to: Verk.WorkersManager, as: :pause
+  defdelegate resume_queue(queue), to: Verk.WorkersManager, as: :resume
+
   @doc """
   Enqueues a Job to the specified queue returning the respective job id
 
