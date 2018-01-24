fd55fc9d610ca8c5ae80328a7dc0545bff19bcdd
Fabian Becker <halfdan@xnorfz.de>
2018-01-24 23:52:17 +0100

    Remove @lint statement
    fixes #147

--- lib/verk/queue_manager.ex
+++ lib/verk/queue_manager.ex
@@ -90,7 +90,6 @@ defmodule Verk.QueueManager do
   end
 
   @doc false
-  @lint {Credo.Check.Refactor.ABCSize, false}
   def handle_call(:enqueue_inprogress, _from, state) do
     in_progress_key = inprogress(state.queue_name, state.node_id)
     case Redix.command(state.redis, ["EVALSHA", @lpop_rpush_src_dest_script_sha, 2,
