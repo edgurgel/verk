17ebdd42a107a06f4274120e84ab3d9dcfbde322
edgurgel <egurgel@sailthru.com>
2018-05-14 09:46:56 +1200

    Bump version 1.2.3
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Verk.Mixfile do
 
   def project do
     [app: :verk,
-     version: "1.2.2",
+     version: "1.2.3",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
