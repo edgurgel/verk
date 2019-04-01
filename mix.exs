c3ecad1cbe51809e50ce27ff92a2e9daa9a5e202
Eduardo Gurgel <eduardo@gurgel.me>
2019-04-01 19:38:41 +1300

    Bump version 1.6.2

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Verk.Mixfile do
   def project do
     [
       app: :verk,
-      version: "1.6.1",
+      version: "1.6.2",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
